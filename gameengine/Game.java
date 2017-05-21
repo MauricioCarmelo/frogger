fedc5227f706da82349eace98bd4b9d0fa8bb2cf
MauricioCarmelo <mauriciocarmelo2@gmail.com>
2017-05-21 18:35:02 -0300

    Game engine pronta

---
+++ gameengine/Game.java
@@ -0,0 +1,129 @@
+package gameengine;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.awt.image.BufferStrategy;
+import java.io.IOException;
+
+import javax.swing.JFrame;
+
+abstract public class Game implements WindowListener{
+	
+	public static final int FRAME_WIDTH = 800;
+	public static final int FRAME_HEIGHT = 600;		
+	
+	private JFrame frame;
+	
+    private BufferStrategy bufferStrategy;
+    
+    boolean running;
+	
+	// construtor
+	public Game() {	
+		frame = new JFrame("Game name");
+		frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+		frame.addWindowListener(this);
+		frame.addKeyListener(InputManager.getObject());		   
+	}	
+	
+	public void loop() throws IOException {
+		
+		running = true;
+		// carregar valores iniciais
+		load();
+		
+		// loop principal do jogo
+		while(running) {
+			// atualizar buffer de entrada do teclado
+			InputManager.getObject().update();
+			// atualizar lógica
+			update();			
+			// pintar na tela
+			draw();
+		}		
+		//terminar o game
+		finish();
+	}
+	
+	public void load() {
+		//frame.setUndecorated(true);
+		//frame.setIgnoreRepaint(true);		
+		frame.setLocation(100, 100);		
+	    frame.setVisible(true);
+	    frame.createBufferStrategy(2);        
+        bufferStrategy = frame.getBufferStrategy();        
+	}
+	
+	public void update() {
+		updateLogic();
+		Thread.yield();
+	}
+	
+	public void draw() throws IOException {
+		
+        Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();        
+        g.setColor(Color.black);        
+        g.fillRect(0, 0, frame.getWidth(), frame.getHeight());
+        
+		onRender(g);
+		
+		g.dispose();
+        bufferStrategy.show();
+	}
+	
+	// terminar o loop
+	public void endLoop() {
+		this.running = false;
+	}
+	
+	// sair do jogo
+	public void finish() {
+		
+		finishGame();		
+		// liberar buffer strategy.
+        bufferStrategy.dispose();
+        // liberar janela
+        frame.dispose();
+	}
+	
+	public int getWidth() {
+		return frame.getWidth();
+	}
+	public int getHeight() {
+		return frame.getHeight();
+	}	
+	
+	// classes abstratas que precisam ser implementadas nas classes filhos.
+	abstract public void onRender(Graphics2D g) throws IOException;
+	abstract public void updateLogic();
+	abstract public void finishGame();
+	
+	// métodos que precisam ser implementados por causa do WindowListener
+	public void windowClosing(WindowEvent e) {
+		this.running = false;
+    }
+	
+	JFrame getMainWindow() {
+		return this.frame;
+	}
+	
+    public void windowOpened(WindowEvent e) {
+    }
+
+    public void windowClosed(WindowEvent e) {
+    }
+
+    public void windowIconified(WindowEvent e) {
+    }
+
+    public void windowDeiconified(WindowEvent e) {
+    }
+
+    public void windowActivated(WindowEvent e) {
+    }   
+    
+    public void windowDeactivated(WindowEvent e) {
+    }	
+}
---
+++ gameengine/InputManager.java
@@ -0,0 +1,135 @@
+package gameengine;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class InputManager implements KeyListener {
+	
+    static protected int KEY_RELEASED = 0;
+    static protected int KEY_JUST_PRESSED = 1;
+    static protected int KEY_PRESSED = 2;
+    static private InputManager instance;
+    private HashMap<Integer, Integer> keyCache;
+    private ArrayList<Integer> pressedKeys;
+    private ArrayList<Integer> releasedKeys;
+
+    private InputManager() {
+        keyCache = new HashMap<Integer, Integer>();
+        pressedKeys = new ArrayList<Integer>();
+        releasedKeys = new ArrayList<Integer>();
+    }
+
+    static public InputManager getObject() {
+        if (instance == null) {
+            instance = new InputManager();
+        }
+        return instance;
+    }
+
+    public boolean isPressed(int key) {
+        return keyCache.containsKey(key)
+                && !keyCache.get(key).equals(KEY_RELEASED);
+    }
+
+    public boolean isJustPressed(int key) {
+        return keyCache.containsKey(key)
+                && keyCache.get(key).equals(KEY_JUST_PRESSED);
+    }
+
+    public boolean isReleased(int key) {
+        return !keyCache.containsKey(key)
+                || keyCache.get(key).equals(KEY_RELEASED);
+    }
+
+    public void update() {
+    	
+        for (Integer keyCode : keyCache.keySet()) {
+            if (isJustPressed(keyCode)) {
+                keyCache.put(keyCode, KEY_PRESSED);
+            }
+        }
+        
+        for (Integer keyCode : releasedKeys) {
+            keyCache.put(keyCode, KEY_RELEASED);
+        }
+        for (Integer keyCode : pressedKeys) {
+            
+        	if (isReleased(keyCode)) {
+                keyCache.put(keyCode, KEY_JUST_PRESSED);
+            } 
+            
+            else {
+                keyCache.put(keyCode, KEY_PRESSED);
+            }
+        }
+        pressedKeys.clear();
+        releasedKeys.clear();
+    }
+
+    public void keyTyped(KeyEvent e) {
+    	
+    }
+
+    public void keyPressed(KeyEvent e) {
+        pressedKeys.add(e.getKeyCode());
+    }
+
+    public void keyReleased(KeyEvent e) {
+        releasedKeys.add(e.getKeyCode());
+    }
+}
+
+/*import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.HashMap;
+
+public class InputManager implements KeyListener {
+	
+	static private InputManager object;
+	
+	// buffer que faz o pooling para o teclado
+	HashMap<Integer, Boolean> keyPool;
+	
+	// construtor
+	private InputManager() {
+		keyPool = new HashMap<Integer, Boolean>();
+	}
+	
+	static public InputManager getObject() {
+		if (object == null ) {
+			object = new InputManager();
+		}
+		return object;
+	}	
+	
+	// método interface alto nível
+	public boolean isPressed(int key) {
+		if( keyPool.get(key) != null ) {
+			return true;
+		}
+		return false;
+		//return keyPool.get(key) != null;
+	}
+	// método interface alto nível
+	public boolean isReleased(int key) {
+		if( keyPool.get(key) == null ) {
+			return true;
+		}
+		return false;
+		//return keyPool.get(key) == null;
+	}	
+	
+	public void keyTyped(KeyEvent e) {
+		
+	}	
+	public void keyPressed(KeyEvent e) {
+		keyPool.put(e.getKeyCode(), true);
+	}
+	
+	public void keyReleased(KeyEvent e) {
+		keyPool.remove(e.getKeyCode());
+	}
+}*/
+
