e71434879a3bc2d77a18d11993546ae23a9f8bf4
mauri <mauri@DESKTOP-LQJGRT0>
2017-06-04 22:23:46 -0300

    moto adicionada

---
+++ frogger/src/mygame/Motorcycle.java
@@ -0,0 +1,13 @@
+package mygame;
+
+public class Motorcycle extends Vehicle {
+
+	public Motorcycle(double x, double y){
+		super(x, y);
+	}
+	
+	public Motorcycle(double x, double y, double velocity){
+		super(x, y, velocity);
+	}
+	
+}
