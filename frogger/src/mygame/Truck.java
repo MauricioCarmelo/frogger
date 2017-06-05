40731c6b3f87dc84f52c95eb2f89b1dda1a8d383
mauri <mauri@DESKTOP-LQJGRT0>
2017-06-04 22:20:09 -0300

    caminhao adicionado

---
+++ frogger/src/mygame/Truck.java
@@ -0,0 +1,13 @@
+package mygame;
+
+public class Truck extends Vehicle {
+
+	public Truck(double x, double y){
+		super(x, y);
+	}
+	
+	public Truck(double x, double y, double velocity){
+		super(x, y, velocity);
+	}
+	
+}
