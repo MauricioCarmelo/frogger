b91ccea0f556a0db8459d63d9666d43df74070ed
mauri <mauri@DESKTOP-LQJGRT0>
2017-06-04 22:37:09 -0300

    simulacao melhor

--- frogger/src/mygame/Frogger.java
+++ frogger/src/mygame/Frogger.java
@@ -20,12 +20,16 @@ public class Frogger extends Game {
 	// variáveis necessárias para o jogo (bastante coisa)	
 	Frog frog;
 	Car car;
+	Truck truck;
+	Motorcycle motorcycle;
 	
 	// construtor
 	public Frogger() {
 		// passa pro sapo o tamanho da janela pra ele se localizar
 		frog = new Frog(getWidth() - Frog.FROG_WIDTH-10, getHeight() - Frog.FROG_HEIGHT-10);
 		car = new Car(getWidth()- 50, getHeight() - 200, 5);
+		truck = new Truck(getWidth()- 50, getHeight() - 300, 3);
+		motorcycle = new Motorcycle(getWidth()- 50, getHeight() - 400, 1);
 
 	}	
 	
@@ -33,14 +37,14 @@ public class Frogger extends Game {
 		
 	}
 	
-    public void finishGame() {
-        // terminar algum som ou algo do tipo        
-    }
+    
     
 	public void updateLogic() {
 		
 		//movimento dos NPCs
 		car.move();
+		truck.move();
+		motorcycle.move();
 		
 		
 		
@@ -104,7 +108,11 @@ public class Frogger extends Game {
 		g.setColor(Color.green);
 		g.draw(new Rectangle2D.Double(frog.getPosX(), frog.getPosY(), Frog.FROG_WIDTH, Frog.FROG_HEIGHT));
 		g.setColor(Color.red);
-		g.draw(new Rectangle2D.Double(car.getPosX(), car.getPosY(), 50, 50));
+		g.draw(new Rectangle2D.Double(car.getPosX(), car.getPosY(), 70, 50));
+		g.setColor(Color.yellow);
+		g.draw(new Rectangle2D.Double(truck.getPosX(), truck.getPosY(), 100, 50));
+		g.setColor(Color.orange);
+		g.draw(new Rectangle2D.Double(motorcycle.getPosX(), motorcycle.getPosY(), 65, 50));
 		drawBackgroud(g);
 	}
 	
@@ -112,4 +120,8 @@ public class Frogger extends Game {
 		
 		
 	}
+	
+	public void finishGame() {
+        // terminar algum som ou algo do tipo        
+    }
 }
